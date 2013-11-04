707033e458ca2a3e4192827d66e306a7252afe9f
Ilya Zverev <zverik@textual.ru>
2013-11-04 11:19:37 +0400

    update build process, update mapbbcode

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 TARGET=dist/mapbbcode_fud.zip
 DIR=mapbbcode
+MAPBBCODE=js
 mkdir $DIR
 cp mapbbcode.plugin $DIR
-cp -r js $DIR
+
+if [ ! -e $MAPBBCODE ]; then
+git clone https://github.com/MapBBCode/mapbbcode.git $MAPBBCODE
+fi
+cd $MAPBBCODE
+git pull origin master
+cd ..
+
+mkdir $DIR/js
+cp -r $MAPBBCODE/dist/lib/* $DIR/js
+cp $MAPBBCODE/dist/mapbbcode.js $DIR/js
+cp $MAPBBCODE/dist/mapbbcode-config.js $DIR/js
+cp $MAPBBCODE/dist/mapbbcode-window.html $DIR/js
+cp b_map.gif $DIR/js/images
 rm $TARGET
 zip -qr $TARGET $DIR
 rm -r $DIR
