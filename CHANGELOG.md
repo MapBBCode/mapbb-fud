39e09760754838ed3abb60297bec3c7722a0082e
Ilya Zverev <zverik@textual.ru>
2013-11-13 23:49:15 +0400

    version 1.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# MapBBCode for FUDForum Changelog
+
+### 1.1.0 (2013-11-13)
+
+* Update MapBBCode to version 1.1.0.
+* Made it easier to enable proprietary layers.
+
+### 1.0.0 (2013-11-03)
+
+Initial version.
--- build.sh
+++ build.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
+DISTFILE=mapbbcode-latest.zip
 TARGET=dist/mapbbcode_fud.zip
 DIR=mapbbcode
-MAPBBCODE=js
+
+wget -nv http://mapbbcode.org/dist/$DISTFILE
+unzip -q $DISTFILE
+rm $DISTFILE
+mv mapbbcode js
+
 mkdir $DIR
 cp mapbbcode.plugin $DIR
-
-if [ ! -e $MAPBBCODE ]; then
-git clone https://github.com/MapBBCode/mapbbcode.git $MAPBBCODE
-fi
-cd $MAPBBCODE
-git pull origin master
-cd ..
-
-mkdir $DIR/js
-cp -r $MAPBBCODE/dist/lib/* $DIR/js
-cp $MAPBBCODE/dist/mapbbcode.js $DIR/js
-cp $MAPBBCODE/dist/mapbbcode-config.js $DIR/js
-cp $MAPBBCODE/dist/mapbbcode-window.html $DIR/js
+mv js $DIR
+rm -r $DIR/js/lang
 cp b_map.gif $DIR/js/images
+
 rm $TARGET
 zip -qr $TARGET $DIR
 rm -r $DIR
